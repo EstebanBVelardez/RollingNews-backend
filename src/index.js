3b2d0327c9d7c93e92644c142b14452bfdbf2228
Esteban Velardez <estebanbvelardez@gmail.com>
2021-05-19 00:41:51 -0300

    se agrego Middlewares. morgan y core, tambien el index html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+//crear una instancia de express
 
 const app = express();
 
-const port = 4000
 
-app.listen(port, ()=>{
-    console.log('Estoy usando el puerto ' + port )
-})
+//crear el puerto
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy usando el puerto '+ app.get('port'));
+});
+
+//herramientas extras - Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '../public')));
+//rutas
+app.get('/',(req,res) => {
+res.send('hola desde el servidor')
+});
