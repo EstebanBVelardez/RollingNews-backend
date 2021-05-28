9e968240d62180863e9c47336624df6937e4b46b
Esteban Velardez <estebanbvelardez@gmail.com>
2021-05-28 19:09:20 -0300

    se actualizo el modelo schema de noticias

--- src/models/noticias.js
+++ src/models/noticias.js
@@ -1,19 +1,35 @@
 import moongose, {Schema} from 'mongoose';
 
 const noticiasSchema = new Schema({
-    categoria: {
+    autor: {
         type: String,
         requiered: true,
     },
-    nombreNoticia:{
+    fecha:{
         type: String,
         requiered:true
     },
-    descripcionNoticia: {
+    titulo: {
         type: String,
         required: true
     },
-    contenidoNoticia:{
+    categoria:{
+        type:String,
+        requiered:true
+    },
+    resumen:{
+        type:String,
+        requiered:true
+    },
+    contenido:{
+        type:String,
+        requiered:true
+    },
+    imgGrande:{
+        type:String,
+        requiered:true
+    },
+    imgChica:{
         type:String,
         requiered:true
     }
