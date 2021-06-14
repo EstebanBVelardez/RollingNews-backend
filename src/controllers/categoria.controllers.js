6f5232d7c164f0ea16e4afeb732859a172c584ff
Esteban Velardez <estebanbvelardez@gmail.com>
2021-06-14 14:38:04 -0300

    se corrigio un error al eliminar y editar categorias

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -37,8 +37,8 @@ res.status(200).json(arregloCategorias);
 
 categoriaCtrl.eliminarCategoria = async(req,res)=>{
 try{
-console.log(req.params.id);
-await Categoria.findByIdAndDelete(req.params.id);
+console.log(req.params.codigo);
+await Categoria.findByIdAndDelete(req.params.codigo);
 res.status(200).json({
   mensaje: "La categoria fue eliminada"
 })
@@ -52,7 +52,7 @@ res.status(200).json({
 
 categoriaCtrl.editarCategoria = async(req,res)=>{
   try{
-await Categoria.findByIdAndUpdate(req.params.id, req.body);
+await Categoria.findByIdAndUpdate(req.params.codigo, req.body);
 res.status(200).json({
   mensaje: "La categoria fue modificada"
 })
@@ -66,7 +66,7 @@ res.status(200).json({
 
 categoriaCtrl.obternerCategoria = async(req,res)=>{
   try{
-const categoriaBuscada = await Categoria.findById(req.params.id);
+const categoriaBuscada = await Categoria.findById(req.params.codigo);
 res.status(200).json(categoriaBuscada)
   }catch(error){
     console.log(error)
